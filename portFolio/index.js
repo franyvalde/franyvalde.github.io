d068e9da733fc64edf57dc6826e4a0185748fd16
franyvalde <79220392+franyvalde@users.noreply.github.com>
2022-05-18 16:44:37 -0300

    Add files via upload

--- portFolio/index.js
+++ portFolio/index.js
@@ -1,24 +1,87 @@
+//titulo
 function Cambiar_Parrafo() {
-    document.getElementById("edit-exp").style.display ="block";
-    let texto = document.getElementById("texto_i").innerText;
+    document.getElementById("edit-titulo").style.display ="block";
+    let texto = document.getElementById("titulo").innerText;
     console.log(texto);
   };
 
 function Definir_Texto(valor){
-  document.getElementById("texto_i").innerText=valor;
+  document.getElementById("titulo").innerText=valor;
 
 }
 function logMessage(message) {
   console.log(message + "<br>");
 }
 //controla si se preciona enter
-let textarea = document.getElementById("edit-exp")
+let textarea = document.getElementById("edit-titulo")
   textarea.addEventListener(`keyup`, (e) => {
+    logMessage(`Key "${e.key}" released [event: keyup]`);
+    if (e.key=="Enter") {
+      document.getElementById("edit-titulo").style.display="none"
+    }
+  });
+
+
+//exp
+function Cambiar_Parrafo2() {
+    document.getElementById("edit-exp").style.display ="block";
+    let texto = document.getElementById("exp").innerText;
+    console.log(texto);
+  };
+
+function Definir_Texto2(valor){
+  document.getElementById("exp").innerText=valor;
+
+}
+
+//controla si se preciona enter
+let textarea2 = document.getElementById("edit-exp")
+  textarea2.addEventListener(`keyup`, (e) => {
     logMessage(`Key "${e.key}" released [event: keyup]`);
     if (e.key=="Enter") {
       document.getElementById("edit-exp").style.display="none"
     }
   });
+
+//edu
+function Cambiar_Parrafo3() {
+  document.getElementById("edit-edu").style.display ="block";
+  let texto = document.getElementById("edu").innerText;
+  console.log(texto);
+};
+
+function Definir_Texto3(valor){
+document.getElementById("edu").innerText=valor;
+
+}
+//controla si se preciona enter
+let textarea3 = document.getElementById("edit-edu")
+textarea3.addEventListener(`keyup`, (e) => {
+  logMessage(`Key "${e.key}" released [event: keyup]`);
+  if (e.key=="Enter") {
+    document.getElementById("edit-edu").style.display="none"
+  }
+});
+
+//hys
+function Cambiar_Parrafo4() {
+  document.getElementById("edit-hys").style.display ="block";
+  let texto = document.getElementById("hys").innerText;
+  console.log(texto);
+};
+
+function Definir_Texto4(valor){
+document.getElementById("hys").innerText=valor;
+
+}
+//controla si se preciona enter
+let textarea4 = document.getElementById("edit-hys")
+textarea4.addEventListener(`keyup`, (e) => {
+  logMessage(`Key "${e.key}" released [event: keyup]`);
+  if (e.key=="Enter") {
+    document.getElementById("edit-hys").style.display="none"
+  }
+});
 
 //carga el contenido de un archivo de texto
 //y llo muestra en el parrafo
